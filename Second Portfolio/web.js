0021e4dba87a43681776b4a88d91f98755ff0efd
Regopotswe Rampa <135703334+RegopotsweRampa@users.noreply.github.com>
2024-03-08 21:35:57 +0200

    Delete Second Portfolio directory

--- Second Portfolio/web.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var menuBtn = document.getElementById("menuBtn");
-    var navbarLinks = document.getElementById("navbarLinks");
-  
-    menuBtn.addEventListener("click", function() {
-      menuBtn.classList.toggle("active");
-      navbarLinks.classList.toggle("active");
-    });
-  });
